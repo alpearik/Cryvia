72323012dc2f34030eb702f2e5c6eb3f231517ee
Alperen <ayaako09002@gmail.com>
2025-07-21 22:40:13 +0200

    feat: replace CMC API with CoinGecko API for price fecthing

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { supabase } from './supabaseClient';
 
 function Dashboard({user}){
-  const API_KEY = import.meta.env.VITE_CMC_API_KEY;
+  //const API_KEY = import.meta.env.VITE_CMC_API_KEY;
 
   const [assets, setAssets] = useState([]);
   const [totalValue, setTotalValue] = useState(0);
@@ -26,21 +26,28 @@ function Dashboard({user}){
     }
   }
   async function calculateTotalValue(assetList) {
-    const symbols = assetList.map(a => a.symbol.toUpperCase()).join(',');
+    const idsMap = {
+      BTC: 'bitcoin',
+      ETH: 'ethereum',
+      SOL: 'solana',
+      USDT: 'tether'
+    };
+    
+    const coinIds = assetList
+    .map(a => idsMap[a.symbol.toUpperCase()])
+    .filter(Boolean)
+    .join(',');
+
 
     try {
-      const res = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbols}`, {
-        headers: {
-          'X-CMC_PRO_API_KEY': API_KEY
-        }
-      });
-
-      const data = await res.json();
+      const res = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coinIds}&vs_currencies=usd`);
+      const result = await res.json();
       
       let total = 0;
       for (let asset of assetList) {
         const symbol = asset.symbol.toUpperCase();
-        const price = data.data[symbol]?.quote?.USD?.price || 0;
+        const id = idsMap[symbol];
+        const price = result[id]?.usd || 0;
         total += asset.amount * price;
       }
 
